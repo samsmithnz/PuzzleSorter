c20cab03cbbe18fbbb8d57fd56a00eca6cd5b551
Sam Smith <samsmithnz@gmail.com>
2023-01-06 21:54:54 -0500

    opened image and counted pixels

--- src/PuzzleSolver.Tests/UnitTest1.cs
+++ src/PuzzleSolver.Tests/UnitTest1.cs
@@ -6,6 +6,13 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        //Arrange
+        Class1 class1 = new();
+        
+        //Act
+        int count = class1.ProcessImage(@"C:\Users\samsm\source\repos\PuzzleSolver\src\PuzzleSolver\baseImage.png");
 
+        //Assert
+        Assert.AreEqual(4, count);
     }
 }
--- src/PuzzleSolver/Class1.cs
+++ src/PuzzleSolver/Class1.cs
@@ -1,5 +1,36 @@
-﻿namespace PuzzleSolver;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace PuzzleSolver;
+
 public class Class1
 {
+    public int ProcessImage(string srcFilename)
+    {
+        var srcFile = new FileInfo(srcFilename);
+        var destFilename = Path.GetFileNameWithoutExtension(srcFile.Name) + "_sorted.jpg";
 
+        using var srcImg = Image.Load<Rgb24>(srcFile.FullName);
+
+        var srcWidth = srcImg.Size().Width;
+        var srcHeight = srcImg.Size().Height;
+
+        //using var destImg = new Image<Rgb24>(srcWidth, srcHeight);
+
+        //for (var row = 0; row < srcHeight; row++)
+        //{
+        //    var pixels = srcImg.GetPixelRowSpan(row).ToArray();
+        //    var orderedPixels = pixels.OrderBy(p => p.R + p.G + p.B).ToArray();
+
+        //    for (var col = 0; col < orderedPixels.Length; col++)
+        //    {
+        //        destImg[col, row] = orderedPixels[col];
+        //    }
+        //}
+
+        //destImg.SaveAsJpeg(destFilename, new JpegEncoder() { Quality = 95 });
+
+        return srcWidth* srcHeight;
+    }
 }
