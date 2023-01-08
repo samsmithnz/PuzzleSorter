65e125c11ed89ac08d36217ac0d7600729cc3aaa
Sam Smith <samsmithnz@gmail.com>
2023-01-07 21:16:34 -0500

    moved test file

--- src/PuzzleSolver.Tests/ImageProcessingTests.cs
+++ src/PuzzleSolver.Tests/ImageProcessingTests.cs
@@ -13,7 +13,7 @@ public class ImageProcessingTests
         ImageProcessing imageProcessing = new();
 
         //Act
-        Dictionary<Rgb24, List<Rgb24>> groupedColors = imageProcessing.ProcessImageIntoColorGroups(@"C:\Users\samsm\source\repos\PuzzleSolver\src\PuzzleSolver\baseImage.png");
+        Dictionary<Rgb24, List<Rgb24>> groupedColors = imageProcessing.ProcessImageIntoColorGroups(@"C:\Users\samsm\source\repos\PuzzleSolver\src\PuzzleSolver.Tests\TestImages\baseImage.png");
 
         //Assert
         Assert.AreEqual(3, groupedColors.Count);
