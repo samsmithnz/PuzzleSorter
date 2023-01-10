c169ae9a2c0d922c110ef15e9a289e0d31a0acf2
Sam Smith <samsmithnz@gmail.com>
2023-01-10 16:30:00 -0500

    Adding new percent field

--- src/PuzzleSolver/ImageProcessing.cs
+++ src/PuzzleSolver/ImageProcessing.cs
@@ -90,4 +90,9 @@ public class ImageProcessing
         return (int)Math.Sqrt(Math.Pow(color1.R - color2.R, 2) + Math.Pow(color1.G - color2.G, 2) + Math.Pow(color1.B - color2.B, 2));
     }
 
+    private static string BuildNamedColorsAndPercents(Dictionary<Rgb24, List<Rgb24>> colorGroups)
+    {
+        return "";
+    }
+
 }
