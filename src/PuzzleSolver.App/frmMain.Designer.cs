59feb055c885a4c813dd880faf44c86ea8d40e04
Sam Smith <samsmithnz@gmail.com>
2023-01-16 15:15:31 -0500

    Added base images to for targets

--- src/PuzzleSolver.App/frmMain.Designer.cs
+++ src/PuzzleSolver.App/frmMain.Designer.cs
@@ -29,11 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.picSourceImage = new System.Windows.Forms.PictureBox();
+            this.imglistTargetImages = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSourceImage)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSourceImage
+            // 
+            this.picSourceImage.Image = global::PuzzleSolver.App.Properties.Resources.st_john_beach;
+            this.picSourceImage.Location = new System.Drawing.Point(21, 22);
+            this.picSourceImage.Name = "picSourceImage";
+            this.picSourceImage.Size = new System.Drawing.Size(1000, 750);
+            this.picSourceImage.TabIndex = 0;
+            this.picSourceImage.TabStop = false;
+            // 
+            // imglistTargetImages
+            // 
+            this.imglistTargetImages.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imglistTargetImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglistTargetImages.ImageStream")));
+            this.imglistTargetImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.imglistTargetImages.Images.SetKeyName(0, "BaseImage.png");
+            this.imglistTargetImages.Images.SetKeyName(1, "ColorfulPhoto.jpg");
+            this.imglistTargetImages.Images.SetKeyName(2, "NamedColors.jpg");
+            this.imglistTargetImages.Images.SetKeyName(3, "PrimaryAndSecondaryColors.png");
+            this.imglistTargetImages.Images.SetKeyName(4, "PuzzlePieces.jpg");
+            this.imglistTargetImages.Images.SetKeyName(5, "RedToBlueBlend.jpg");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 778);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 200);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "   Red";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1352, 1629);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picSourceImage);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Puzzle Solver tester";
+            ((System.ComponentModel.ISupportInitialize)(this.picSourceImage)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox picSourceImage;
+        private ImageList imglistTargetImages;
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
     }
 }
--- src/PuzzleSolver.App/frmMain.cs
+++ src/PuzzleSolver.App/frmMain.cs
@@ -1,3 +1,6 @@
+using SixLabors.ImageSharp.PixelFormats;
+using System.Diagnostics;
+
 namespace PuzzleSolver.App
 {
     public partial class frmMain : Form
@@ -5,6 +8,38 @@ namespace PuzzleSolver.App
         public frmMain()
         {
             InitializeComponent();
+
+            List<Rgb24> palette = ColorPalettes.GetPrimaryAndSecondaryColorsPalette();
+
+            int startingX = 21;
+            int startingY = 778;
+            int containerHeight = 200;
+            int containerWidth = 400;
+            for (int i = 0; i < palette.Count; i++)
+            {
+                Rgb24 item = palette[i];
+                int x = startingX * ((i / 4) + containerWidth);
+                int y = (i * containerHeight) + (startingY + 20);
+                Debug.WriteLine("X:" + x + ",Y:" + y);
+                GroupBox groupBox = new()
+                {
+                    Height = containerHeight,
+                    Width = containerWidth,
+                    Text = "   " + ColorPalettes.ToName(item),
+                    Location = new Point(x, y),
+                    Parent = this
+                };
+
+                _ = new PictureBox()
+                {
+                    Location = new Point(6, 8),
+                    Height = 20,
+                    Width = 20,
+                    BackColor = Color.FromName(ColorPalettes.ToName(item)),
+                    Parent = groupBox
+                };
+
+            }
         }
     }
 }
