5634f64d248f6978fd8f346b42ce7f6232c84c41
Sam Smith <samsmithnz@gmail.com>
2023-01-21 16:27:39 -0500

    Reduced the number of containers

--- src/PuzzleSolver.App/frmMain.cs
+++ src/PuzzleSolver.App/frmMain.cs
@@ -56,81 +56,98 @@ namespace PuzzleSolver.App
             {
                 foreach (KeyValuePair<Rgb24, List<Rgb24>> item in sourceImageStats.ColorGroups)
                 {
-
-                    int x = containerStartingX;
-                    int y = (i * containerHeight) + (i * containerStartingY) + containerStartingY;
-                    //Create the groupbox container for the parent color
-                    GroupBox groupBox = new()
+                    if (CheckIfColorGroupUsed(item.Key, subImages))
                     {
-                        Height = containerHeight,
-                        Width = containerWidth,
-                        Text = "   " + ColorPalettes.ToName(item.Key),
-                        Location = new System.Drawing.Point(x, y),
-                        Parent = panColors,
-                        Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left
-                    };
-                    //Create a panel to sit in the group - this will allow us to add a horizontal scrollbar
-                    Panel panel = new()
-                    {
-                        Location = new System.Drawing.Point(3, 34),
-                        Size = new System.Drawing.Size(794, 430),
-                        Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right,
-                        AutoScroll = true,
-                        Parent = groupBox
-                    };
-                    //Create a image with the color that lives in the title of the groupbox
-                    _ = new PictureBox()
-                    {
-                        Location = new System.Drawing.Point(6, 8),
-                        Height = 20,
-                        Width = 20,
-                        BackColor = System.Drawing.Color.FromName(ColorPalettes.ToName(item.Key)),
-                        Parent = groupBox
-                    };
-
-                    //Find all child images matching the top grouping spot
-                    int xLocation = 0;
-                    for (int j = 0; j < subImages.Count; j++)
-                    {
-                        Debug.WriteLine(subImages[j].TopColorGroupColor);
-                        if (item.Key == subImages[j].TopColorGroupColor)
+                        int x = containerStartingX;
+                        int y = (i * containerHeight) + (i * containerStartingY) + containerStartingY;
+                        //Create the groupbox container for the parent color
+                        GroupBox groupBox = new()
                         {
-                            //Now we have to show the items that map to this parent
-                            _ = new Label()
+                            Height = containerHeight,
+                            Width = containerWidth,
+                            Text = "   " + ColorPalettes.ToName(item.Key),
+                            Location = new System.Drawing.Point(x, y),
+                            Parent = panColors,
+                            Anchor = AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left
+                        };
+                        //Create a panel to sit in the group - this will allow us to add a horizontal scrollbar
+                        Panel panel = new()
+                        {
+                            Location = new System.Drawing.Point(3, 34),
+                            Size = new System.Drawing.Size(794, 430),
+                            Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right,
+                            AutoScroll = true,
+                            Parent = groupBox
+                        };
+                        //Create a image with the color that lives in the title of the groupbox
+                        _ = new PictureBox()
+                        {
+                            Location = new System.Drawing.Point(6, 8),
+                            Height = 20,
+                            Width = 20,
+                            BackColor = System.Drawing.Color.FromName(ColorPalettes.ToName(item.Key)),
+                            Parent = groupBox
+                        };
+
+                        //Find all child images matching the top grouping spot
+                        int xLocation = 0;
+                        for (int j = 0; j < subImages.Count; j++)
+                        {
+                            Debug.WriteLine(subImages[j].TopColorGroupColor);
+                            if (item.Key == subImages[j].TopColorGroupColor)
                             {
-                                Location = new System.Drawing.Point(5 + (250 * xLocation + (20 * xLocation)), 10),
-                                Size = new System.Drawing.Size(100, 30),
-                                Text = "#" + (j + 1).ToString(),
-                                Parent = panel
-                            };
-                            _ = new PictureBox()
-                            {
-                                Location = new System.Drawing.Point(5 + (250 * xLocation + (20 * xLocation)), 43), //5 + 250 for each column, with a 20 buffer for each column too.
-                                Width = subImageWidth,
-                                Height = subImageHeight,
-                                Image = bitmaps[j],
-                                BorderStyle = BorderStyle.FixedSingle,
-                                Parent = panel
-                            };
-                            string text = subImages[j].NamesToString;
-                            _ = new Label()
-                            {
-                                AutoSize = false,
-                                Location = new System.Drawing.Point(6 + (250 * xLocation + (20 * xLocation)), 296),
-                                Height = 100,
-                                Width = 250,
-                                Text = text,
-                                Parent = panel
-                            };
-                            xLocation++;
+                                //Now we have to show the items that map to this parent
+                                _ = new Label()
+                                {
+                                    Location = new System.Drawing.Point(5 + (250 * xLocation + (20 * xLocation)), 10),
+                                    Size = new System.Drawing.Size(100, 30),
+                                    Text = "#" + (j + 1).ToString(),
+                                    Parent = panel
+                                };
+                                _ = new PictureBox()
+                                {
+                                    Location = new System.Drawing.Point(5 + (250 * xLocation + (20 * xLocation)), 43), //5 + 250 for each column, with a 20 buffer for each column too.
+                                    Width = subImageWidth,
+                                    Height = subImageHeight,
+                                    Image = bitmaps[j],
+                                    BorderStyle = BorderStyle.FixedSingle,
+                                    Parent = panel
+                                };
+                                string text = subImages[j].NamesToString;
+                                _ = new Label()
+                                {
+                                    AutoSize = false,
+                                    Location = new System.Drawing.Point(6 + (250 * xLocation + (20 * xLocation)), 296),
+                                    Height = 100,
+                                    Width = 250,
+                                    Text = text,
+                                    Parent = panel
+                                };
+                                xLocation++;
+                            }
                         }
+                        i++;
                     }
-                    i++;
                 }
             }
 
 
         }
+
+        private bool CheckIfColorGroupUsed(Rgb24 color, List<ImageStats> subImages)
+        {
+            bool result = false;
+            foreach (ImageStats item in subImages)
+            {
+                if (item.TopColorGroupColor == color)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
 
         private List<Bitmap> SplitBitmapIntoPieces(System.Drawing.Image sourceImage, int width, int height)
         {
