cfb45f7e10ce5bb5a371092fce4b593ba73f24ed
Sam Smith <samsmithnz@gmail.com>
2023-01-28 10:26:35 -0500

    fixed .net standard 2.0 downgrade bug

--- src/PuzzleSolver/ImageStats.cs
+++ src/PuzzleSolver/ImageStats.cs
@@ -22,7 +22,7 @@ namespace PuzzleSolver
         {
             get
             {
-                return ColorGroups?.FirstOrDefault().Key;
+                return ColorGroups.FirstOrDefault().Key;
             }
         }
 
