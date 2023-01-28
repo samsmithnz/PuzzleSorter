dc6ec32979aaaca74bc95c8846aee6c634020bc4
Sam Smith <samsmithnz@gmail.com>
2023-01-28 10:12:35 -0500

    removed y axis

--- src/PuzzleSolver/Map/MapCore.cs
+++ src/PuzzleSolver/Map/MapCore.cs
@@ -30,7 +30,7 @@ namespace Battle.Logic.Map
         }
 
 
-        public static List<Vector3> GetMapArea(string[,] map, Vector3 sourceLocation, int range, bool lookingForFOV = true, bool includeSourceLocation = false)
+        public static List<Vector2> GetMapArea(string[,] map, Vector2 sourceLocation, int range, bool lookingForFOV = true, bool includeSourceLocation = false)
         {
             int startingX = (int)sourceLocation.X;
             int startingZ = (int)sourceLocation.Z;
@@ -58,25 +58,25 @@ namespace Battle.Logic.Map
             }
 
             //Get a list of all border squares
-            HashSet<Vector3> borderTiles = new HashSet<Vector3>();
+            HashSet<Vector2> borderTiles = new HashSet<Vector2>();
             //Add the top and bottom rows
             for (int x = minX; x <= maxX; x++)
             {
-                borderTiles.Add(new Vector3(x, 0, minZ));
-                borderTiles.Add(new Vector3(x, 0, maxZ));
+                borderTiles.Add(new Vector2(x, 0, minZ));
+                borderTiles.Add(new Vector2(x, 0, maxZ));
             }
             //Add the left and right sides
             for (int z = minZ; z < maxZ; z++)
             {
-                borderTiles.Add(new Vector3(minX, 0, z));
-                borderTiles.Add(new Vector3(maxX, 0, z));
+                borderTiles.Add(new Vector2(minX, 0, z));
+                borderTiles.Add(new Vector2(maxX, 0, z));
             }
 
             //For each border tile, draw a line from the starting point to the border
-            HashSet<Vector3> results = new HashSet<Vector3>();
-            //foreach (Vector3 borderItem in borderTiles)
+            HashSet<Vector2> results = new HashSet<Vector2>();
+            //foreach (Vector2 borderItem in borderTiles)
             //{
-            //    List<Vector3> singleLineCheck = FieldOfView.GetPointsOnLine(new Vector3(startingX, 0, startingZ), borderItem);
+            //    List<Vector2> singleLineCheck = FieldOfView.GetPointsOnLine(new Vector2(startingX, 0, startingZ), borderItem);
             //    if (singleLineCheck.Count > 0 &&
             //        singleLineCheck[singleLineCheck.Count - 1].X == startingX &&
             //        singleLineCheck[singleLineCheck.Count - 1].Z == startingZ)
@@ -90,7 +90,7 @@ namespace Battle.Logic.Map
             //    for (int i = 0; i < singleLineCheck.Count; i++)
             //    {
             //        currentLength += lineSegment;
-            //        Vector3 fovItem = singleLineCheck[i];
+            //        Vector2 fovItem = singleLineCheck[i];
             //        if (fovItem.X >= 0 && fovItem.Y >= 0 && fovItem.Z >= 0)
             //        {
             //            //If we find an object, stop adding tiles
@@ -125,7 +125,7 @@ namespace Battle.Logic.Map
             return results.ToList();
         }
 
-        public static double GetLengthOfLine(Vector3 start, Vector3 end, int decimals = 0)
+        public static double GetLengthOfLine(Vector2 start, Vector2 end, int decimals = 0)
         {
             double lineLength = Math.Sqrt(Math.Pow((end.X - start.X), 2) + Math.Pow((end.Z - start.Z), 2));
             return Math.Round(lineLength, decimals);
@@ -171,9 +171,9 @@ namespace Battle.Logic.Map
             return sb.ToString();
         }
 
-        public static string[,] ApplyListToMap(string[,] map, List<Vector3> list, string tile)
+        public static string[,] ApplyListToMap(string[,] map, List<Vector2> list, string tile)
         {
-            foreach (Vector3 item in list)
+            foreach (Vector2 item in list)
             {
                 //Check that the square is empty - we don't want to overwrite something that exists and only put a tile on an unused tile
                 if (map[(int)item.X, (int)item.Z] == "")
@@ -184,23 +184,23 @@ namespace Battle.Logic.Map
             return map;
         }
 
-        public static string[,] ApplyListToExistingMap(string[,] map, List<Vector3> list, string tile)
+        public static string[,] ApplyListToExistingMap(string[,] map, List<Vector2> list, string tile)
         {
-            foreach (Vector3 item in list)
+            foreach (Vector2 item in list)
             {
                 map[(int)item.X, (int)item.Z] = tile;
             }
             return map;
         }
 
-        public static string GetMapStringWithItems(string[,] map, List<Vector3> list)
+        public static string GetMapStringWithItems(string[,] map, List<Vector2> list)
         {
             string[,] mapNew = MapCore.ApplyListToMap((string[,])map.Clone(), list, "o");
             string mapString = MapCore.GetMapString(mapNew);
             return mapString;
         }
 
-        public static string GetMapStringWithItemLayers(string[,] map, List<Vector3> baseList, List<Vector3> overlayList)
+        public static string GetMapStringWithItemLayers(string[,] map, List<Vector2> baseList, List<Vector2> overlayList)
         {
             string[,] mapNew = MapCore.ApplyListToMap((string[,])map.Clone(), baseList, "o");
             mapNew = MapCore.ApplyListToExistingMap(mapNew, overlayList, "O");
@@ -250,7 +250,7 @@ namespace Battle.Logic.Map
             return sb.ToString();
         }
 
-        public static List<Vector2> FindAdjacentTile(string[,] map, Vector3 currentLocation, string tileToFind)
+        public static List<Vector2> FindAdjacentTile(string[,] map, Vector2 currentLocation, string tileToFind)
         {
             int width = map.GetLength(0);
             //int height = map.GetLength(1);
