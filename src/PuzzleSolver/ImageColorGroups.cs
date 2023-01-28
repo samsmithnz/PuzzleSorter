28162365b0505fa0d91fb776903c6343262f3bc8
Sam Smith <samsmithnz@gmail.com>
2023-01-28 10:23:50 -0500

    fixed .net standard 2.0 downgrade bug

--- src/PuzzleSolver/ImageColorGroups.cs
+++ src/PuzzleSolver/ImageColorGroups.cs
@@ -3,183 +3,184 @@ using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Collections.Generic;
 
-namespace PuzzleSolver;
-
-public class ImageColorGroups
+namespace PuzzleSolver
 {
-    public SortedList<int, Rgb24> PriorityColorPalette { get; set; }
-    public List<Rgb24> ColorPalette { get; set; }
-
-    public ImageColorGroups(List<Rgb24> colorPalette, SortedList<int, Rgb24> priorityColorPalette = null)
+    public class ImageColorGroups
     {
-        ColorPalette = colorPalette;
-        if (priorityColorPalette == null)
-        {
-            PriorityColorPalette = new SortedList<int, Rgb24>();
-        }
-        else
-        {
-            PriorityColorPalette = priorityColorPalette;
-        }
-    }
+        public SortedList<int, Rgb24> PriorityColorPalette { get; set; }
+        public List<Rgb24> ColorPalette { get; set; }
 
-    /// <summary>
-    /// Get Images Statistics for target image
-    /// </summary>
-    /// <param name="sourceFilename"></param>
-    /// <param name="image"></param>
-    /// <param name="onlyShowTop3"></param>
-    /// <returns></returns>
-    public ImageStats ProcessStatsForImage(string sourceFilename = null, Image<Rgb24> image = null, bool onlyShowTop3 = true)
-    {
-        ImageStats imageStats = null;
-        Image<Rgb24> sourceImage = null;
-        if (sourceFilename != null)
+        public ImageColorGroups(List<Rgb24> colorPalette, SortedList<int, Rgb24> priorityColorPalette = null)
         {
-            sourceImage = Image.Load<Rgb24>(sourceFilename);
-        }
-        else if (image != null)
-        {
-            sourceImage = image;
-        }
-        
-        if (sourceImage != null)
-        {
-            imageStats = new ImageStats(sourceImage)
+            ColorPalette = colorPalette;
+            if (priorityColorPalette == null)
             {
-                ColorGroups = ProcessImageIntoColorGroups(sourceImage),
-                PriorityColorPalette = PriorityColorPalette
-            };
-            imageStats.NamedColorsAndPercentList = BuildNamedColorsAndPercentList(imageStats.ColorGroups, imageStats.PriorityColorPalette, onlyShowTop3);
-        }
-        return imageStats;
-    }
-
-    private Dictionary<Rgb24, List<Rgb24>> ProcessImageIntoColorGroups(Image<Rgb24> image)
-    {
-        Dictionary<Rgb24, List<Rgb24>> groupedColors = new();
-
-        image.ProcessPixelRows(accessor =>
-        {
-            //int srcWidth = sourceImg.Size().Width;
-            int srcHeight = image.Size().Height;
-
-            for (var row = 0; row < srcHeight; row++)
+                PriorityColorPalette = new SortedList<int, Rgb24>();
+            }
+            else
             {
-                Span<Rgb24> pixelSpan = accessor.GetRowSpan(row);
-                for (var col = 0; col < pixelSpan.Length; col++)
+                PriorityColorPalette = priorityColorPalette;
+            }
+        }
+
+        /// <summary>
+        /// Get Images Statistics for target image
+        /// </summary>
+        /// <param name="sourceFilename"></param>
+        /// <param name="image"></param>
+        /// <param name="onlyShowTop3"></param>
+        /// <returns></returns>
+        public ImageStats ProcessStatsForImage(string sourceFilename = null, Image<Rgb24> image = null, bool onlyShowTop3 = true)
+        {
+            ImageStats imageStats = null;
+            Image<Rgb24> sourceImage = null;
+            if (sourceFilename != null)
+            {
+                sourceImage = Image.Load<Rgb24>(sourceFilename);
+            }
+            else if (image != null)
+            {
+                sourceImage = image;
+            }
+
+            if (sourceImage != null)
+            {
+                imageStats = new ImageStats(sourceImage)
                 {
-                    Rgb24 colorGroup = FindClosestColorGroup(pixelSpan[col]);
-                    if (colorGroup != null)
+                    ColorGroups = ProcessImageIntoColorGroups(sourceImage),
+                    PriorityColorPalette = PriorityColorPalette
+                };
+                imageStats.NamedColorsAndPercentList = BuildNamedColorsAndPercentList(imageStats.ColorGroups, imageStats.PriorityColorPalette, onlyShowTop3);
+            }
+            return imageStats;
+        }
+
+        private Dictionary<Rgb24, List<Rgb24>> ProcessImageIntoColorGroups(Image<Rgb24> image)
+        {
+            Dictionary<Rgb24, List<Rgb24>> groupedColors = new();
+
+            image.ProcessPixelRows(accessor =>
+            {
+                //int srcWidth = sourceImg.Size().Width;
+                int srcHeight = image.Size().Height;
+
+                for (var row = 0; row < srcHeight; row++)
+                {
+                    Span<Rgb24> pixelSpan = accessor.GetRowSpan(row);
+                    for (var col = 0; col < pixelSpan.Length; col++)
                     {
-                        if (!groupedColors.ContainsKey((Rgb24)colorGroup))
+                        Rgb24 colorGroup = FindClosestColorGroup(pixelSpan[col]);
+                        if (colorGroup != null)
                         {
-                            List<Rgb24> colorList = new()
+                            if (!groupedColors.ContainsKey((Rgb24)colorGroup))
                             {
+                                List<Rgb24> colorList = new()
+                                {
                                 pixelSpan[col]
-                            };
-                            groupedColors[(Rgb24)colorGroup] = colorList;
-                        }
-                        else
-                        {
-                            List<Rgb24> colorList = groupedColors[(Rgb24)colorGroup];
-                            colorList.Add(pixelSpan[col]);
-                            groupedColors[(Rgb24)colorGroup] = colorList;
+                                };
+                                groupedColors[(Rgb24)colorGroup] = colorList;
+                            }
+                            else
+                            {
+                                List<Rgb24> colorList = groupedColors[(Rgb24)colorGroup];
+                                colorList.Add(pixelSpan[col]);
+                                groupedColors[(Rgb24)colorGroup] = colorList;
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
 
-        //Order with the most number of colors rolling up into the parent color first.
-        groupedColors = groupedColors.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+            //Order with the most number of colors rolling up into the parent color first.
+            groupedColors = groupedColors.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
 
-        return groupedColors;
-    }
-
-    //Group RGB colors into multiple groups
-    private Rgb24 FindClosestColorGroup(Rgb24 colorToTest)
-    {
-        Rgb24 closestColorGroup = null;
-        List<KeyValuePair<Rgb24, int>> results = new();
-        foreach (Rgb24 color in ColorPalette)
-        {
-            int colorDifference = GetColorDifference(color, colorToTest);
-            results.Add(new KeyValuePair<Rgb24, int>(color, colorDifference));
+            return groupedColors;
         }
-        //Order the results and save over itself
-        results = results.OrderBy(t => t.Value).ToList();
 
-        //Check if the largest positive or negative value is closer
-        if (results.Count > 0)
+        //Group RGB colors into multiple groups
+        private Rgb24 FindClosestColorGroup(Rgb24 colorToTest)
         {
-            closestColorGroup = results[0].Key;
-        }
-        return closestColorGroup;
-    }
-
-    //Since it uses Sqrt, it always returns a positive number
-    private static int GetColorDifference(Rgb24 color1, Rgb24 color2)
-    {
-        return (int)Math.Sqrt(Math.Pow(color1.R - color2.R, 2) + Math.Pow(color1.G - color2.G, 2) + Math.Pow(color1.B - color2.B, 2));
-    }
-
-    private static List<ColorStats> BuildNamedColorsAndPercentList(
-        Dictionary<Rgb24, List<Rgb24>> colorGroups,
-        SortedList<int, Rgb24> priorityColorPalette,
-        bool onlyShowTop3)
-    {
-        List<ColorStats> roughNamePercentList = new();
-        //Calculate the name and percent and add it into a list
-        foreach (KeyValuePair<Rgb24, List<Rgb24>> colorGroup in colorGroups)
-        {
-            double percent = (double)colorGroup.Value.Count / (double)colorGroups.Sum(t => t.Value.Count);
-            roughNamePercentList.Add(new(colorGroup.Key, ColorPalettes.ToName(colorGroup.Key), percent));
-        }
-        //If there are priority items, update the order
-        if (priorityColorPalette.Count > 0)
-        {
-            foreach (KeyValuePair<int, Rgb24> priorityItem in priorityColorPalette)
+            Rgb24 closestColorGroup = null;
+            List<KeyValuePair<Rgb24, int>> results = new();
+            foreach (Rgb24 color in ColorPalette)
             {
-                ColorStats priorityColorStats = roughNamePercentList.Where(x => x.Rgb == priorityItem.Value).FirstOrDefault();
-                if (priorityColorStats != null)
+                int colorDifference = GetColorDifference(color, colorToTest);
+                results.Add(new KeyValuePair<Rgb24, int>(color, colorDifference));
+            }
+            //Order the results and save over itself
+            results = results.OrderBy(t => t.Value).ToList();
+
+            //Check if the largest positive or negative value is closer
+            if (results.Count > 0)
+            {
+                closestColorGroup = results[0].Key;
+            }
+            return closestColorGroup;
+        }
+
+        //Since it uses Sqrt, it always returns a positive number
+        private static int GetColorDifference(Rgb24 color1, Rgb24 color2)
+        {
+            return (int)Math.Sqrt(Math.Pow(color1.R - color2.R, 2) + Math.Pow(color1.G - color2.G, 2) + Math.Pow(color1.B - color2.B, 2));
+        }
+
+        private static List<ColorStats> BuildNamedColorsAndPercentList(
+            Dictionary<Rgb24, List<Rgb24>> colorGroups,
+            SortedList<int, Rgb24> priorityColorPalette,
+            bool onlyShowTop3)
+        {
+            List<ColorStats> roughNamePercentList = new();
+            //Calculate the name and percent and add it into a list
+            foreach (KeyValuePair<Rgb24, List<Rgb24>> colorGroup in colorGroups)
+            {
+                double percent = (double)colorGroup.Value.Count / (double)colorGroups.Sum(t => t.Value.Count);
+                roughNamePercentList.Add(new(colorGroup.Key, ColorPalettes.ToName(colorGroup.Key), percent));
+            }
+            //If there are priority items, update the order
+            if (priorityColorPalette.Count > 0)
+            {
+                foreach (KeyValuePair<int, Rgb24> priorityItem in priorityColorPalette)
                 {
-                    priorityColorStats.Order = priorityItem.Key;
+                    ColorStats priorityColorStats = roughNamePercentList.Where(x => x.Rgb == priorityItem.Value).FirstOrDefault();
+                    if (priorityColorStats != null)
+                    {
+                        priorityColorStats.Order = priorityItem.Key;
+                    }
                 }
             }
-        }
-        //Order the percent list
-        roughNamePercentList = roughNamePercentList.OrderBy(t => t.Order).ThenByDescending(x => x.Percent).ThenBy(x => x.Name).ToList();
+            //Order the percent list
+            roughNamePercentList = roughNamePercentList.OrderBy(t => t.Order).ThenByDescending(x => x.Percent).ThenBy(x => x.Name).ToList();
 
-        //Add the other percent if needed
-        List<ColorStats> finalNamePercentList = new();
-        if (onlyShowTop3 == true)
-        {
-            int count = 0;
-            double totalOtherPercent = 0;
-            foreach (ColorStats item in roughNamePercentList)
+            //Add the other percent if needed
+            List<ColorStats> finalNamePercentList = new();
+            if (onlyShowTop3 == true)
             {
-                if (onlyShowTop3 == true && count < 2)
+                int count = 0;
+                double totalOtherPercent = 0;
+                foreach (ColorStats item in roughNamePercentList)
                 {
-                    finalNamePercentList.Add(item);
-                    count++;
+                    if (onlyShowTop3 == true && count < 2)
+                    {
+                        finalNamePercentList.Add(item);
+                        count++;
+                    }
+                    else
+                    {
+                        totalOtherPercent += item.Percent;
+                    }
                 }
-                else
+                //If there is an other percent over 0, add it
+                if (Math.Round(totalOtherPercent, 2) > 0)
                 {
-                    totalOtherPercent += item.Percent;
+                    finalNamePercentList.Add(new(null, "Other", totalOtherPercent));
                 }
             }
-            //If there is an other percent over 0, add it
-            if (Math.Round(totalOtherPercent, 2) > 0)
+            else
             {
-                finalNamePercentList.Add(new(null, "Other", totalOtherPercent));
+                finalNamePercentList = roughNamePercentList;
             }
-        }
-        else
-        {
-            finalNamePercentList = roughNamePercentList;
-        }
 
-        return finalNamePercentList;
+            return finalNamePercentList;
+        }
     }
 }
