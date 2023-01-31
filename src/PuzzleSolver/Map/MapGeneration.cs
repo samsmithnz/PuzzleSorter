44c0e3bfb9517c6a2e0db0ef11754c8621fce2e5
Sam Smith <samsmithnz@gmail.com>
2023-01-31 17:56:07 -0500

    Removed y axis

--- src/PuzzleSolver/Map/MapGeneration.cs
+++ src/PuzzleSolver/Map/MapGeneration.cs
@@ -4,37 +4,36 @@ namespace PuzzleSolver.Map
 {
     public static class MapGeneration
     {
-        public static string[,,] GenerateMap()
+        public static string[,] GenerateMap()
         {
-            string[,,] map = MapCore.InitializeMap(5, 1, 5);
+            string[,] map = MapCore.InitializeMap(5, 5);
             int width = map.GetLength(0);
-            int breadth = map.GetLength(2);
-            int y = 0;
-            for (int z = 0; z < breadth; z++)
+            int height = map.GetLength(1);
+            for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    if (x == 0 && z == 0 ||
-                        x == 0 && z == 2 ||
-                        x == 0 && z == 4 ||
-                        x == 2 && z == 0 ||
-                        x == 2 && z == 4 ||
-                        x == 4 && z == 0 ||
-                        x == 4 && z == 2 ||
-                        x == 4 && z == 4)
+                    if (x == 0 && y == 0 ||
+                        x == 0 && y == 2 ||
+                        x == 0 && y == 4 ||
+                        x == 2 && y == 0 ||
+                        x == 2 && y == 4 ||
+                        x == 4 && y == 0 ||
+                        x == 4 && y == 2 ||
+                        x == 4 && y == 4)
                     {
-                        //map[x, y, z] = "D"; //drop zone
+                        //map[x, y] = "D"; //drop zone
                     }
-                    else if (x == 2 && z == 2)
+                    else if (x == 2 && y == 2)
                     {
-                        map[x, y, z] = "P"; //pickup zone
+                        map[x, y] = "P"; //pickup zone
                     }
                 }
             }
             return map;
         }
 
-        public static void DebugPrintOutMap(string[,,] map)
+        public static void DebugPrintOutMap(string[,] map)
         {
             int width = map.GetLength(0);
             int breadth = map.GetLength(1);
