7d1780aad4396c0a7884dee664a54d97a9e38a81
Sam Smith <samsmithnz@gmail.com>
2023-02-03 17:55:10 -0500

    added base initialization class

--- src/PuzzleSolver/Board.cs
+++ src/PuzzleSolver/Board.cs
@@ -9,5 +9,19 @@ namespace PuzzleSolver
         public string[,] Map { get; set; }
         public Vector2 UnsortedPileLocation { get; set; }
         public Dictionary<Vector2, Rgb24> SortedPileLocations { get; set; }
+
+
+        public Board(int width, int height, Vector2 unsortedPileLocation, List<Rgb24> colorPalette)
+        {
+            Map = new string[width, height];
+            UnsortedPileLocation = unsortedPileLocation;
+            SortedPileLocations = new Dictionary<Vector2, Rgb24>();
+            //int i = 0;
+            //foreach (Rgb24 color in colorPalette)
+            //{
+            //    SortedPileLocations.Add(new Vector2(i, 0), color);
+            //    i++;
+            //}
+        }
     }
 }
