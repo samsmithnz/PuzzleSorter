b7476b07eba2a9611d8b6d7167712d29f582a8e5
Sam Smith <samsmithnz@gmail.com>
2023-02-04 20:06:47 -0500

    Added location to robot

--- src/PuzzleSolver/Robots/Robot.cs
+++ src/PuzzleSolver/Robots/Robot.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Numerics;
 
 namespace PuzzleSolver.Robots
 {
     public class Robot
     {
+        public Vector2 Location { get; set; }
     }
 }
