f32289ff57fd9e4ef2fd2f03d52119363dd5fe4c
Sam Smith <samsmithnz@gmail.com>
2023-02-05 14:37:41 -0500

    code gardening

---
+++ src/PuzzleSolver/SortedPiece.cs
@@ -0,0 +1,11 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using System.Numerics;
+
+namespace PuzzleSolver
+{
+    public class SortedPiece
+    {
+        public Rgb24 Color { get; set; }
+        public Vector2 Location { get; set; }
+    }
+}
