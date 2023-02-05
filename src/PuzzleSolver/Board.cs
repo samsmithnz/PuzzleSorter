f93e8898e2f090697c9f8006061a337c689c74a6
Sam Smith <samsmithnz@gmail.com>
2023-02-04 20:21:44 -0500

    revised board

--- src/PuzzleSolver/Board.cs
+++ src/PuzzleSolver/Board.cs
@@ -9,19 +9,30 @@ namespace PuzzleSolver
     public class Board
     {
         public string[,] Map { get; set; }
-        public Vector2 UnsortedPileLocation { get; set; }
-        public Dictionary<Vector2, Rgb24> SortedPileLocations { get; set; }
-        public Queue<Image<Rgb24>> UnsortedPieces { get; set; }
-        public Robot Robot { get; set; }
         
+        //Pieces
+        public Vector2 UnsortedPiecesLocation { get; set; }
+        public Queue<Image<Rgb24>> UnsortedPieces { get; set; }
+        public Dictionary<Vector2, Rgb24> SortedPiecesLocations { get; set; }
+        public int SortedPiecesCount { get; set; }
+        public int UnsortedPiecesCount
+        {
+            get
+            {
+                return UnsortedPieces.Count;
+            }
+        }
+
+        public Robot Robot { get; set; }
+
 
         public Board() { }
 
         public Board(int width, int height, Vector2 unsortedPileLocation, List<Rgb24> colorPalette)
         {
             Map = new string[width, height];
-            UnsortedPileLocation = unsortedPileLocation;
-            SortedPileLocations = new Dictionary<Vector2, Rgb24>();
+            UnsortedPiecesLocation = unsortedPileLocation;
+            SortedPiecesLocations = new Dictionary<Vector2, Rgb24>();
             //int i = 0;
             //foreach (Rgb24 color in colorPalette)
             //{
