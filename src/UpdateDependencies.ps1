48478f183811fd3b3c728f1192c194c178a26d7b
Sam Smith <samsmithnz@gmail.com>
2023-02-06 21:43:48 -0500

    Updated depenedencies

--- src/UpdateDependencies.ps1
+++ src/UpdateDependencies.ps1
@@ -1,13 +1,19 @@
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/samsmithnz/puzzlesolver/releases/latest/download/PuzzleSolver.Logic.deps.json -OutFile c:\users\samsm\downloads\PuzzleSolver.Logic.deps.json
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/samsmithnz/puzzlesolver/releases/latest/download/PuzzleSolver.Logic.pdb -OutFile c:\users\samsm\downloads\PuzzleSolver.Logic.pdb
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/samsmithnz/puzzlesolver/releases/latest/download/PuzzleSolver.Logic.dll -OutFile c:\users\samsm\downloads\PuzzleSolver.Logic.dll
+cls
+$latest = Invoke-WebRequest "https://api.github.com/repos/samsmithnz/PuzzleSolver/releases/latest"
+$tagName = ($latest.Content | ConvertFrom-Json).tag_name
+Write-Host "Downloading files for release $tagName"
 
-Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.Logic.deps.json
-Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.Logic.pdb
-Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.Logic.dll
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/samsmithnz/PuzzleSolver/releases/download/$tagName/PuzzleSolver.deps.json" -OutFile c:\users\samsm\downloads\PuzzleSolver.deps.json
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/samsmithnz/PuzzleSolver/releases/download/$tagName/PuzzleSolver.pdb" -OutFile c:\users\samsm\downloads\PuzzleSolver.pdb
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/samsmithnz/PuzzleSolver/releases/download/$tagName/PuzzleSolver.dll" -OutFile c:\users\samsm\downloads\PuzzleSolver.dll
 
-Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.Logic.deps.json -Destination C:\Users\samsm\source\repos\TBS\TBS\Assets\PuzzleSolverDependencies -Force
-Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.Logic.pdb -Destination C:\Users\samsm\source\repos\TBS\TBS\Assets\PuzzleSolverDependencies -Force
-Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.Logic.dll -Destination C:\Users\samsm\source\repos\TBS\TBS\Assets\PuzzleSolverDependencies -Force
+
+Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.dll
+Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.pdb
+Unblock-File -Path c:\users\samsm\downloads\PuzzleSolver.deps.json
+
+Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.dll -Destination C:\Users\samsm\source\repos\PuzzleSolver\src\Puzzle3dSimulation\Assets\PuzzleSolverDependencies -Force
+Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.pdb -Destination C:\Users\samsm\source\repos\PuzzleSolver\src\Puzzle3dSimulation\Assets\PuzzleSolverDependencies -Force
+Move-Item -Path c:\users\samsm\downloads\PuzzleSolver.deps.json -Destination C:\Users\samsm\source\repos\PuzzleSolver\src\Puzzle3dSimulation\Assets\PuzzleSolverDependencies -Force
 
 pause
