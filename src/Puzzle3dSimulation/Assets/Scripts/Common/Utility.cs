54fe7485ce3858c6a2c72777046026b412590ca1
Sam Smith <samsmithnz@gmail.com>
2023-02-07 09:38:21 -0500

    code gardening

--- src/Puzzle3dSimulation/Assets/Scripts/Common/Utility.cs
+++ src/Puzzle3dSimulation/Assets/Scripts/Common/Utility.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Assets.Scripts.Common
 {
@@ -17,45 +16,45 @@ namespace Assets.Scripts.Common
         public const string TAG_DestructibleTerrain = "DestructibleTerrain";
         public const string TAG_TargetingCrosshair = "TargetingCrosshair";
 
-        public static UnityEngine.Vector3 ConvertToUnity3DV3(System.Numerics.Vector3 vector3)
-        {
-            return new UnityEngine.Vector3(vector3.X, vector3.Y, vector3.Z);
-        }
+        //public static UnityEngine.Vector3 ConvertToUnity3DV3(System.Numerics.Vector3 vector3)
+        //{
+        //    return new UnityEngine.Vector3(vector3.X, vector3.Y, vector3.Z);
+        //}
 
-        public static System.Numerics.Vector3 ConvertToNumericV3(UnityEngine.Vector3 vector3)
-        {
-            return new System.Numerics.Vector3(vector3.x, vector3.y, vector3.z);
-        }
+        //public static System.Numerics.Vector3 ConvertToNumericV3(UnityEngine.Vector3 vector3)
+        //{
+        //    return new System.Numerics.Vector3(vector3.x, vector3.y, vector3.z);
+        //}
 
-        public static List<UnityEngine.Vector3> ConvertToUnity3DV3List(List<System.Numerics.Vector3> vector3List)
-        {
-            List<UnityEngine.Vector3> results = new List<UnityEngine.Vector3>();
-            foreach (System.Numerics.Vector3 item in vector3List)
-            {
-                results.Add(ConvertToUnity3DV3(item));
-            }
-            return results;
-        }
+        //public static List<UnityEngine.Vector3> ConvertToUnity3DV3List(List<System.Numerics.Vector3> vector3List)
+        //{
+        //    List<UnityEngine.Vector3> results = new List<UnityEngine.Vector3>();
+        //    foreach (System.Numerics.Vector3 item in vector3List)
+        //    {
+        //        results.Add(ConvertToUnity3DV3(item));
+        //    }
+        //    return results;
+        //}
 
-        public static List<KeyValuePair<Vector3, int>> ConvertToUnity3DV3List(List<KeyValuePair<System.Numerics.Vector3, int>> vector3List)
-        {
-            List<KeyValuePair<Vector3, int>> results = new List<KeyValuePair<Vector3, int>>();
-            foreach (KeyValuePair<System.Numerics.Vector3, int> item in vector3List)
-            {
-                results.Add(new KeyValuePair<Vector3, int>(ConvertToUnity3DV3(item.Key), item.Value));
-            }
-            return results;
-        }
+        //public static List<KeyValuePair<Vector3, int>> ConvertToUnity3DV3List(List<KeyValuePair<System.Numerics.Vector3, int>> vector3List)
+        //{
+        //    List<KeyValuePair<Vector3, int>> results = new List<KeyValuePair<Vector3, int>>();
+        //    foreach (KeyValuePair<System.Numerics.Vector3, int> item in vector3List)
+        //    {
+        //        results.Add(new KeyValuePair<Vector3, int>(ConvertToUnity3DV3(item.Key), item.Value));
+        //    }
+        //    return results;
+        //}
 
-        public static List<System.Numerics.Vector3> ConvertToNumericV3List(List<UnityEngine.Vector3> vector3List)
-        {
-            List<System.Numerics.Vector3> results = new List<System.Numerics.Vector3>();
-            foreach (UnityEngine.Vector3 item in vector3List)
-            {
-                results.Add(ConvertToNumericV3(item));
-            }
-            return results;
-        }
+        //public static List<System.Numerics.Vector3> ConvertToNumericV3List(List<UnityEngine.Vector3> vector3List)
+        //{
+        //    List<System.Numerics.Vector3> results = new List<System.Numerics.Vector3>();
+        //    foreach (UnityEngine.Vector3 item in vector3List)
+        //    {
+        //        results.Add(ConvertToNumericV3(item));
+        //    }
+        //    return results;
+        //}
 
         public static string CreateName(string prefix, Vector2 location)
         {
