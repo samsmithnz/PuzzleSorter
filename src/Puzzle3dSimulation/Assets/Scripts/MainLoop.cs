27181a8da58c332a08d09d27596e52cd9e4f037a
Sam Smith <samsmithnz@gmail.com>
2023-02-07 08:40:43 -0500

    fixed errors

--- src/Puzzle3dSimulation/Assets/Scripts/MainLoop.cs
+++ src/Puzzle3dSimulation/Assets/Scripts/MainLoop.cs
@@ -21,10 +21,10 @@ public class MainLoop : MonoBehaviour
                     SixLabors.ImageSharp.Color.Green.ToPixel<Rgb24>() }),
             SortedPieces = new Dictionary<Rgb24, SortedPiece>()
                 {
-                    { SixLabors.ImageSharp.Color.Red.ToPixel<Rgb24>(), new(SixLabors.ImageSharp.Color.Red.ToPixel<Rgb24>(),new System.Numerics.Vector2(0, 0))},
-                    { SixLabors.ImageSharp.Color.Blue.ToPixel<Rgb24>(), new(SixLabors.ImageSharp.Color.Blue.ToPixel<Rgb24>(),new System.Numerics.Vector2(0, 4))},
-                    { SixLabors.ImageSharp.Color.Green.ToPixel<Rgb24>(), new(SixLabors.ImageSharp.Color.Green.ToPixel<Rgb24>(),new System.Numerics.Vector2(4, 0))},
-                    { SixLabors.ImageSharp.Color.Yellow.ToPixel<Rgb24>(), new(SixLabors.ImageSharp.Color.Yellow.ToPixel<Rgb24>(),new System.Numerics.Vector2(4, 4))}
+                    { SixLabors.ImageSharp.Color.Red.ToPixel<Rgb24>(), new SortedPiece(SixLabors.ImageSharp.Color.Red.ToPixel<Rgb24>(),new System.Numerics.Vector2(0, 0))},
+                    { SixLabors.ImageSharp.Color.Blue.ToPixel<Rgb24>(), new SortedPiece(SixLabors.ImageSharp.Color.Blue.ToPixel<Rgb24>(),new System.Numerics.Vector2(0, 4))},
+                    { SixLabors.ImageSharp.Color.Green.ToPixel<Rgb24>(), new SortedPiece(SixLabors.ImageSharp.Color.Green.ToPixel<Rgb24>(),new System.Numerics.Vector2(4, 0))},
+                    { SixLabors.ImageSharp.Color.Yellow.ToPixel<Rgb24>(), new SortedPiece(SixLabors.ImageSharp.Color.Yellow.ToPixel<Rgb24>(),new System.Numerics.Vector2(4, 4))}
                 },
             SortedPiecesCount = 0,
             Robot = new Robot(new System.Numerics.Vector2(2, 1))
