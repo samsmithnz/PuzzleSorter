87325357e0559e89f4dc715066d82090789c2c39
Sam Smith <samsmithnz@gmail.com>
2023-02-07 19:05:45 -0500

    Added new piece

---
+++ src/PuzzleSolver/Piece.cs
@@ -0,0 +1,20 @@
+﻿using PuzzleSolver.Images;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace PuzzleSolver
+{
+    public class Piece
+    {
+        public int Id { get; set; }
+        public Image<Rgb24> Image { get; set; }
+        public ImageStats ImageStats { get; set; }
+        public Rgb24 TopColorGroup
+        {
+            get
+            {
+                return ImageStats.TopColorGroupColor;
+            }
+        }
+    }
+}
