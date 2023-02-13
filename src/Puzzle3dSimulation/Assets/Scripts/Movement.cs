8d936ed261ec00b904bd5bb729f4dc0564dce949
Sam Smith <samsmithnz@gmail.com>
2023-02-12 20:02:24 -0500

    Added more movement code

--- src/Puzzle3dSimulation/Assets/Scripts/Movement.cs
+++ src/Puzzle3dSimulation/Assets/Scripts/Movement.cs
@@ -5,17 +5,85 @@ using UnityEngine.TextCore.Text;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public IEnumerator MovePiece()
     {
-        
+        yield return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator MoveRobot()
     {
-        
+        yield return null;
     }
+
+    //public IEnumerator MoveRobot(System.Numerics.Vector3 source, 
+    //    System.Numerics.Vector3 destination, 
+    //    MainLoop mainLoopScript, 
+    //    Team opponentTeam)
+    //{
+    //    //Setup the movement
+    //    Character character = mainLoopScript.CurrentCharacter.CharacterLogic;
+    //    Team characterTeam = mainLoopScript.CurrentCharacter.CharacterTeam;
+    //    GameObject characterGameObject = mainLoopScript.CurrentCharacter.CharacterGameObject;
+    //    MoveObject moveObjectScript = gameObject.GetComponent<MoveObject>();
+    //    if (moveObjectScript == null)
+    //    {
+    //        moveObjectScript = gameObject.AddComponent<MoveObject>();
+    //    }
+
+    //    //Get the movement logic
+    //    List<MovementAction> movementActions = mainLoopScript.Mission.MoveCharacter(character, characterTeam, opponentTeam, destination);
+
+    //    //Center the camera over the character (Don't put a start coroutine here)
+    //    mainLoopScript.MoveCameraOverPlayer(Utility.ConvertToUnity3DV3(source));
+    //    //Move both the camera and the character
+    //    mainLoopScript.LockCameraToCurrentPlayer = true;
+    //    //Perform the movement
+    //    Utility.LogWithTime("Moving from " + source.ToString() + " to " + destination.ToString());
+    //    foreach (MovementAction item in movementActions)
+    //    {
+    //        Utility.LogWithTime(item.Log[0]);
+    //        Vector3 adjustedStartLocation = Utility.ConvertToUnity3DV3(item.StartLocation);
+    //        adjustedStartLocation.y = 1f;
+    //        Vector3 adjustedEndLocation = Utility.ConvertToUnity3DV3(item.EndLocation);
+    //        adjustedEndLocation.y = 1f;
+    //        yield return StartCoroutine(moveObjectScript.MoveObjectWithNoRotation(characterGameObject.transform,
+    //              adjustedStartLocation,
+    //              adjustedEndLocation,
+    //              0.25f));
+    //        mainLoopScript.CurrentCharacter.CharacterLogic.SetLocationAndRange(mainLoopScript.Mission.Map, item.EndLocation, mainLoopScript.CurrentCharacter.CharacterLogic.FOVRange, opponentTeam.Characters);
+    //        mainLoopScript.UpdateFOV();
+
+    //        //if this move results in an overwatch encounter
+    //        if (item.OverwatchEncounterResults != null && item.OverwatchEncounterResults.Count > 0)
+    //        {
+    //            Utility.LogWithTime("Overwatch results for " + item.EndLocation.ToString() + " " + item.OverwatchEncounterResults.Count);
+    //            foreach (EncounterResult overwatchEncounter in item.OverwatchEncounterResults)
+    //            {
+    //                //Log the event
+    //                Utility.LogWithTime(overwatchEncounter.LogString);
+
+    //                //zoom behind character
+    //                Character3D sourceCharacter = Utility.FindCharacter3dObject(overwatchEncounter.SourceCharacter.Name, mainLoopScript.CharacterGameObjectList);
+    //                Character3D targetCharacter = Utility.FindCharacter3dObject(overwatchEncounter.TargetCharacter.Name, mainLoopScript.CharacterGameObjectList);
+    //                mainLoopScript.MoveCameraBehindPlayerToAim(sourceCharacter, targetCharacter);
+
+    //                //take overwatch shot
+    //                yield return StartCoroutine(mainLoopScript.ShootAtTarget(sourceCharacter, targetCharacter));
+    //            }
+
+    //            //zoom out and over character
+    //            mainLoopScript.MoveCameraBackOverPlayer();
+    //        }
+    //    }
+    //    mainLoopScript.LockCameraToCurrentPlayer = false;
+
+    //    mainLoopScript.UpdateTargetCharacterList();
+    //    if (character.ActionPointsCurrent <= 0 || character.HitpointsCurrent <= 0)
+    //    {
+    //        //Check if turn is over for current team
+    //        mainLoopScript.CheckCharacterTurn();
+    //    }
+    //}
 }
 
 
@@ -31,72 +99,7 @@ public class Movement : MonoBehaviour
 //{
 //    public class Movement : MonoBehaviour
 //    {
-//        public IEnumerator Move(System.Numerics.Vector3 source, System.Numerics.Vector3 destination, MainLoop mainLoopScript, Team opponentTeam)
-//        {
-//            //Setup the movement
-//            Character character = mainLoopScript.CurrentCharacter.CharacterLogic;
-//            Team characterTeam = mainLoopScript.CurrentCharacter.CharacterTeam;
-//            GameObject characterGameObject = mainLoopScript.CurrentCharacter.CharacterGameObject;
-//            MoveObject moveObjectScript = gameObject.GetComponent<MoveObject>();
-//            if (moveObjectScript == null)
-//            {
-//                moveObjectScript = gameObject.AddComponent<MoveObject>();
-//            }
 
-//            //Get the movement logic
-//            List<MovementAction> movementActions = mainLoopScript.Mission.MoveCharacter(character, characterTeam, opponentTeam, destination);
-
-//            //Center the camera over the character (Don't put a start coroutine here)
-//            mainLoopScript.MoveCameraOverPlayer(Utility.ConvertToUnity3DV3(source));
-//            //Move both the camera and the character
-//            mainLoopScript.LockCameraToCurrentPlayer = true;
-//            //Perform the movement
-//            Utility.LogWithTime("Moving from " + source.ToString() + " to " + destination.ToString());
-//            foreach (MovementAction item in movementActions)
-//            {
-//                Utility.LogWithTime(item.Log[0]);
-//                Vector3 adjustedStartLocation = Utility.ConvertToUnity3DV3(item.StartLocation);
-//                adjustedStartLocation.y = 1f;
-//                Vector3 adjustedEndLocation = Utility.ConvertToUnity3DV3(item.EndLocation);
-//                adjustedEndLocation.y = 1f;
-//                yield return StartCoroutine(moveObjectScript.MoveObjectWithNoRotation(characterGameObject.transform,
-//                      adjustedStartLocation,
-//                      adjustedEndLocation,
-//                      0.25f));
-//                mainLoopScript.CurrentCharacter.CharacterLogic.SetLocationAndRange(mainLoopScript.Mission.Map, item.EndLocation, mainLoopScript.CurrentCharacter.CharacterLogic.FOVRange, opponentTeam.Characters);
-//                mainLoopScript.UpdateFOV();
-
-//                //if this move results in an overwatch encounter
-//                if (item.OverwatchEncounterResults != null && item.OverwatchEncounterResults.Count > 0)
-//                {
-//                    Utility.LogWithTime("Overwatch results for " + item.EndLocation.ToString() + " " + item.OverwatchEncounterResults.Count);
-//                    foreach (EncounterResult overwatchEncounter in item.OverwatchEncounterResults)
-//                    {
-//                        //Log the event
-//                        Utility.LogWithTime(overwatchEncounter.LogString);
-
-//                        //zoom behind character
-//                        Character3D sourceCharacter = Utility.FindCharacter3dObject(overwatchEncounter.SourceCharacter.Name, mainLoopScript.CharacterGameObjectList);
-//                        Character3D targetCharacter = Utility.FindCharacter3dObject(overwatchEncounter.TargetCharacter.Name, mainLoopScript.CharacterGameObjectList);
-//                        mainLoopScript.MoveCameraBehindPlayerToAim(sourceCharacter, targetCharacter);
-
-//                        //take overwatch shot
-//                        yield return StartCoroutine(mainLoopScript.ShootAtTarget(sourceCharacter, targetCharacter));
-//                    }
-
-//                    //zoom out and over character
-//                    mainLoopScript.MoveCameraBackOverPlayer();
-//                }
-//            }
-//            mainLoopScript.LockCameraToCurrentPlayer = false;
-
-//            mainLoopScript.UpdateTargetCharacterList();
-//            if (character.ActionPointsCurrent <= 0 || character.HitpointsCurrent <= 0)
-//            {
-//                //Check if turn is over for current team
-//                mainLoopScript.CheckCharacterTurn();
-//            }
-//        }
 
 //    }
 //}
