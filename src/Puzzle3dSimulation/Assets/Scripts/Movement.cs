a50e7689ee1147d3e9f0c81db10b49ab96ae772c
Sam Smith <samsmithnz@gmail.com>
2023-02-14 08:45:34 -0500

    fixed jumping

--- src/Puzzle3dSimulation/Assets/Scripts/Movement.cs
+++ src/Puzzle3dSimulation/Assets/Scripts/Movement.cs
@@ -19,25 +19,29 @@ public class Movement : MonoBehaviour
             {
                 moveObjectScript = robotObject.AddComponent<MoveObject>();
             }
-            Debug.LogWarning("Position 0: (" + startLocation.X.ToString() + "," + startLocation.Y.ToString() + ")");
+            Debug.LogWarning("Position 0: (" + startLocation.X.ToString() + "," + startLocation.Y.ToString() + ") for " + path.Path.Count + " tiles");
 
             float robotY = 0.5f;
             Vector3 start = Utility.ConvertToUnity3DV3(startLocation, robotY);
-            Vector3 end = Utility.ConvertToUnity3DV3(path.Path[1], robotY);
-            for (int i = 0; i < path.Path.Count - 1; i++)
+            Vector3 end = Utility.ConvertToUnity3DV3(path.Path[0], robotY);
+            yield return StartCoroutine(moveObjectScript.MoveObjectWithNoRotation(robotObject.transform,
+              start,
+              end,
+              1f));
+
+            for (int i = 0; i < path.Path.Count; i++)
             {
-                if (i > 0 && i < path.Path.Count - 1)
+                if (i < path.Path.Count - 1)
                 {
                     start = Utility.ConvertToUnity3DV3(path.Path[i], robotY);
                     end = Utility.ConvertToUnity3DV3(path.Path[i + 1], robotY);
                     Debug.LogWarning("Position " + i + ": (" + end.x.ToString() + "," + end.z.ToString() + ")");
+                    //Debug.LogWarning("Moving from " + start.ToString() + " to " + end.ToString());
+                    yield return StartCoroutine(moveObjectScript.MoveObjectWithNoRotation(robotObject.transform,
+                                start,
+                                end,
+                                1f));
                 }
-                //Debug.LogWarning("Moving from " + start.ToString() + " to " + end.ToString());
-                yield return StartCoroutine(moveObjectScript.MoveObjectWithNoRotation(robotObject.transform,
-                          start,
-                          end,
-                          1f));
-
             }
         }
         yield return null;
