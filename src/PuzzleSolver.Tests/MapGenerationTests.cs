04506829c2c24b3e7b792fe4bd8c95489e3ce4ed
Sam Smith <samsmithnz@gmail.com>
2023-02-19 09:32:56 -0500

    Added more tests

--- src/PuzzleSolver.Tests/MapGenerationTests.cs
+++ src/PuzzleSolver.Tests/MapGenerationTests.cs
@@ -19,6 +19,21 @@ namespace PuzzleSolver.Tests
             
             //Assert
             Assert.AreEqual(25, map.Length);
+            Assert.AreEqual("P", map[2, 2]);
+        }
+
+        [TestMethod]
+        public void GenerateBiggerMapTest()
+        {
+            //Arrange
+            string[,] map = MapGeneration.GenerateMap(9,9);
+
+            //Act
+            MapGeneration.DebugPrintOutMap(map);
+
+            //Assert
+            Assert.AreEqual(81, map.Length);
+            Assert.AreEqual("P", map[5, 5]);
         }
     }
 }
