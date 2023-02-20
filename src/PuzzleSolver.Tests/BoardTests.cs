75e2d2c98d1f8a7d11039c3c074e3bb488caa5b4
Sam Smith <samsmithnz@gmail.com>
2023-02-19 20:06:35 -0500

    updated test

--- src/PuzzleSolver.Tests/BoardTests.cs
+++ src/PuzzleSolver.Tests/BoardTests.cs
@@ -328,13 +328,12 @@ namespace PuzzleSolver.Tests
             Assert.IsNotNull(robotAction1.PathToDropoff);
             Assert.AreEqual(new(2, 1), robotAction1.RobotDropoffStartingLocation);
             Assert.AreEqual(new(1, 1), robotAction1.PathToDropoff.Path[0]);
-            Assert.AreEqual(new(1, 2), robotAction1.PathToDropoff.Path[1]);
-            Assert.AreEqual(new(0, 2), robotAction1.PathToDropoff.Path[2]);
-            Assert.AreEqual(new(0, 3), robotAction1.PathToDropoff.Path[3]);
-            Assert.AreEqual(new(0, 3), robotAction1.RobotDropoffEndingLocation);
+            Assert.AreEqual(new(0, 1), robotAction1.PathToDropoff.Path[1]);
+            Assert.AreEqual(new(0, 0), robotAction1.PathToDropoff.Path[2]);
+            Assert.AreEqual(new(0, 0), robotAction1.RobotDropoffEndingLocation);
             Assert.AreEqual(1, robotAction1.DropoffPieceCount);
             Assert.IsNotNull(robotAction1.DropoffAction);
-            Assert.AreEqual(new(0, 4), board.SortedPieces[0].Location);
+            Assert.AreEqual(new(0, 1), board.SortedPieces[0].Location);
             Assert.AreEqual(10, results.Count);
 
             //Complete the second action
