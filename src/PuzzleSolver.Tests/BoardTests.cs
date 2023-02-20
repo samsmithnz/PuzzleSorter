e3e92f45f59a9a7a8ffe9ed08d0c59dc25f556b4
Sam Smith <samsmithnz@gmail.com>
2023-02-19 20:04:18 -0500

    updated test

--- src/PuzzleSolver.Tests/BoardTests.cs
+++ src/PuzzleSolver.Tests/BoardTests.cs
@@ -217,7 +217,7 @@ namespace PuzzleSolver.Tests
             Assert.IsNotNull(robotAction3);
             Assert.IsNotNull(robotAction3.PieceId);
             Assert.IsNotNull(robotAction3.PathToPickup);
-            Assert.AreEqual(new(4, 1), robotAction3.RobotPickupStartingLocation);
+            Assert.AreEqual(new(1, 2), robotAction3.RobotPickupStartingLocation);
             Assert.AreEqual(new(3, 1), robotAction3.PathToPickup.Path[0]);
             Assert.AreEqual(new(2, 1), robotAction3.PathToPickup.Path[1]);
             Assert.AreEqual(new(2, 1), robotAction3.RobotPickupEndingLocation);
