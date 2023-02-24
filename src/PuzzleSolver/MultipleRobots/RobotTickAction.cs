9f21394510f532b923eaf242dc68125190ca0c67
Sam Smith <samsmithnz@gmail.com>
2023-02-24 07:14:04 -0500

    Added new tick action

--- src/PuzzleSolver/MultipleRobots/RobotTickAction.cs
+++ src/PuzzleSolver/MultipleRobots/RobotTickAction.cs
@@ -1,4 +1,3 @@
-using PuzzleSolver.Map;
 using PuzzleSolver.Processing;
 using System.Collections.Generic;
 using System.Numerics;
@@ -9,6 +8,7 @@ namespace PuzzleSolver
     {
         public int RobotID { get; set; }
         public List<Vector2> Movement { get; set; } = null;
-        public ObjectInteraction Action { get; set; } = null;
+        public ObjectInteraction PickupAction { get; set; } = null;
+        public ObjectInteraction DropoffAction { get; set; } = null;
     }
 }
