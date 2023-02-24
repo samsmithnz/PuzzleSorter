ecd4bedcaea50cfb0e7c6e148de338c5539c859f
Sam Smith <samsmithnz@gmail.com>
2023-02-24 06:51:27 -0500

    updated indexer

--- src/Puzzle3dSimulation/Assets/Scripts/MainLoop.cs
+++ src/Puzzle3dSimulation/Assets/Scripts/MainLoop.cs
@@ -105,14 +105,14 @@ public class MainLoop : MonoBehaviour
         //Add the robot
         Utility.LogWithTime("Creating robot entities");
         List<Rgb24> robotPalette = ColorPalettes.Get3ColorPalette();
-        for (int i = 0; i < board.Robots.Count; i++)
+        for (int j = 0; j < board.Robots.Count; j++)
         {
-            Robot robot = board.Robots[i];
+            Robot robot = board.Robots[j];
             _RobotObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             _RobotObject.transform.position = new Vector3(robot.Location.X, 0.5f, robot.Location.Y);
             _RobotObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             _RobotObject.name = "robot_" + robot.RobotId;
-            _RobotObject.GetComponent<Renderer>().material.color = Utility.ConvertToUnityColor(robotPalette[i]); //UnityEngine.Color.gray; //dark gray
+            _RobotObject.GetComponent<Renderer>().material.color = Utility.ConvertToUnityColor(robotPalette[j]); //UnityEngine.Color.gray; //dark gray
         }
 
         ////Add lines on puzzle map, Drawing line renderers
