a62b866a369472f7ae358dc737b319e58129562b
Sam Smith <samsmithnz@gmail.com>
2023-02-26 09:34:16 -0500

    updated dependencies

--- src/PuzzleSolver/MultipleRobots/RobotTickAction.cs
+++ src/PuzzleSolver/MultipleRobots/RobotTickAction.cs
@@ -7,7 +7,7 @@ namespace PuzzleSolver
     public class RobotTickAction
     {
         public int RobotId { get; set; }
-        public int? PieceId { get; set; }
+        public int PieceId { get; set; }
         public List<Vector2> Movement { get; set; } = null;
         public ObjectInteraction PickupAction { get; set; } = null;
         public ObjectInteraction DropoffAction { get; set; } = null;
