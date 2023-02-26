6354017c7fcb9bf159a26de04d3d9af09f434d10
Sam Smith <samsmithnz@gmail.com>
2023-02-26 09:18:39 -0500

    updated test

--- src/PuzzleSolver.Tests/BoardTests.cs
+++ src/PuzzleSolver.Tests/BoardTests.cs
@@ -558,7 +558,7 @@ namespace PuzzleSolver.Tests
            
             Tick tick4 = results.Ticks[3];
             Assert.AreEqual(4, tick4.TickNumber);
-            Assert.AreEqual(2, tick4.RobotActions.Count);
+            Assert.AreEqual(1, tick4.RobotActions.Count);
             Assert.AreEqual(3, tick4.RobotActions[0].PieceId);
             Assert.AreEqual(2, tick4.RobotActions[0].Movement.Count);
       
