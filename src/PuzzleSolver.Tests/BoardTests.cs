69e4deb26bf4f94111677d7d6c650e328f2d8bc7
Sam Smith <samsmithnz@gmail.com>
2023-03-04 14:41:21 -0500

    fix to tests

--- src/PuzzleSolver.Tests/BoardTests.cs
+++ src/PuzzleSolver.Tests/BoardTests.cs
@@ -763,7 +763,7 @@ namespace PuzzleSolver.Tests
             Assert.AreEqual(11, turn11.TurnNumber);
             Assert.AreEqual(2, turn11.RobotActions.Count);
             Assert.AreEqual(5, turn11.RobotActions[0].PieceId); 
-            Assert.AreEqual(8, turn11.RobotActions[1].PieceId);
+            Assert.AreEqual(7, turn11.RobotActions[1].PieceId);
 
             //check the fourth to last turn
             Assert.AreEqual(19, results.Turns[results.Turns.Count - 4].TurnNumber);
