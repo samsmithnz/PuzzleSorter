c6f730eb75131708d57595738757a603e1270583
Sam Smith <samsmithnz@gmail.com>
2023-03-12 13:52:52 -0400

    updated tests

--- src/PuzzleSolver.Tests/BoardL0Tests.cs
+++ src/PuzzleSolver.Tests/BoardL0Tests.cs
@@ -874,7 +874,7 @@ namespace PuzzleSolver.Tests
             Assert.AreEqual(0, board.UnsortedPieces.Count);
             Assert.AreEqual(10, board.SortedPieces.Count);
             Assert.IsNotNull(results);
-            Assert.AreEqual(61, results.Turns.Count);
+            Assert.AreEqual(66, results.Turns.Count);
 
             ////Turn 25, robot 2 picks up piece 6, and then loses it
             //Turn turn25 = results.Turns[24];
