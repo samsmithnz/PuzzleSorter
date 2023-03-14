96aecda4c1ce0f1a298dfaa68bc6912b256f5536
Sam Smith <samsmithnz@gmail.com>
2023-03-14 15:56:49 -0400

    turn4

--- src/PuzzleSolver.Tests/BoardL0Tests.cs
+++ src/PuzzleSolver.Tests/BoardL0Tests.cs
@@ -1352,8 +1352,8 @@ namespace PuzzleSolver.Tests
             Turn turn4 = results.Turns[3];
             Assert.AreEqual(4, turn4.TurnNumber);
             Assert.AreEqual(2, turn4.RobotActions.Count);
-            Assert.AreEqual(1, turn4.RobotActions[0].RobotId);
-            Assert.AreEqual(2, turn4.RobotActions[1].RobotId);
+            Assert.AreEqual(2, turn4.RobotActions[0].RobotId);
+            Assert.AreEqual(1, turn4.RobotActions[1].RobotId);
             Assert.AreEqual(4, turn4.RobotActions[0].PieceId);
             Assert.AreEqual(null, turn4.RobotActions[1].PieceId);
             Assert.AreEqual(new Vector2(1, 1), turn4.RobotActions[1].Movement[0]);
