267f4406a1d1b463a6cda62eb79eae0ca5837d31
Sam Smith <samsmithnz@gmail.com>
2023-03-19 14:49:04 -0400

    updated tests

--- src/PuzzleSolver.Tests/BoardL0Tests.cs
+++ src/PuzzleSolver.Tests/BoardL0Tests.cs
@@ -1075,7 +1075,7 @@ namespace PuzzleSolver.Tests
             Assert.AreEqual(8, turn8.TurnNumber);
             Assert.AreEqual(2, turn8.RobotActions.Count);
             Assert.AreEqual(null, turn8.RobotActions[0].PieceId);
-            Assert.AreEqual(null, turn8.RobotActions[1].PieceId);
+            Assert.AreEqual(4, turn8.RobotActions[1].PieceId);
             Assert.AreEqual(new Vector2(1, 2), turn8.RobotActions[0].Movement[0]);
             Assert.AreEqual(new Vector2(1, 2), turn8.RobotActions[0].Movement[1]);
             Assert.AreEqual(new Vector2(1, 1), turn8.RobotActions[1].Movement[0]);
