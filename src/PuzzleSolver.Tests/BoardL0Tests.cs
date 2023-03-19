bd5dcc9ff19613684487c0c438db4e5dd9c1254b
Sam Smith <samsmithnz@gmail.com>
2023-03-19 14:42:10 -0400

    updated tests

--- src/PuzzleSolver.Tests/BoardL0Tests.cs
+++ src/PuzzleSolver.Tests/BoardL0Tests.cs
@@ -692,7 +692,7 @@ namespace PuzzleSolver.Tests
             Assert.AreEqual(0, board.UnsortedPieces.Count);
             Assert.AreEqual(10, board.SortedPieces.Count);
             Assert.IsNotNull(results);
-            Assert.AreEqual(62, results.Turns.Count);
+            Assert.AreEqual(54, results.Turns.Count);
 
             //check the first turn
             Turn turn1 = results.Turns[0];
